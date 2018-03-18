418c37b20469347a99aa077118ac16c2ddf16586
Maxim Tsoy <maks.tsoy@gmail.com>
2018-03-18 22:19:51 +0100

    refactor code to satisfy Visual C++ compiler (it doesn't support inline variables in C code)

--- kociemba/build_ckociemba.py
+++ kociemba/build_ckociemba.py
@@ -4,19 +4,19 @@ ffi = cffi.FFI()
 ffi.set_source(
     "kociemba.ckociembawrapper",
     """
-    #include <stdio.h>
-    #include <stdlib.h>
-    #include <search.h>
+    #include "search.h"
 
     char* solve(char *cubestring, char *patternstring, char *cache_dir)
     {
         char patternized[64];
+        char *sol;
+
         if (patternstring) {
             patternize(cubestring, patternstring, patternized);
             cubestring = patternized;
         }
 
-        char *sol = solution(
+        sol = solution(
             cubestring,
             24,
             1000,
@@ -28,11 +28,14 @@ ffi.set_source(
     """,
     include_dirs=['kociemba/ckociemba/include'],
     sources=[
+        'kociemba/ckociemba/prunetable_helpers.c',
         'kociemba/ckociemba/coordcube.c',
         'kociemba/ckociemba/cubiecube.c',
         'kociemba/ckociemba/facecube.c',
-        'kociemba/ckociemba/search.c'],
-    extra_compile_args=['-std=c99', '-O3', '-D_XOPEN_SOURCE=700'])
+        'kociemba/ckociemba/search.c'
+    ],
+    extra_compile_args=['-std=c99', '-O3', '-D_XOPEN_SOURCE=700']
+)
 
 ffi.cdef("char* solve(char *cubestring, char *patternstring, char *cache_dir);")
 
